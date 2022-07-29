08645e6fdc0bcd5f0fbb730006bb8d0db3f96469
Yessica Juarez <yessica48206@gmail.com>
2022-07-28 22:32:13 -0300

    comienzo del trabajo practico numero 2

---
+++ js/punto1.js
@@ -0,0 +1,8 @@
+//1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje
+// que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+let edad= parseInt(prompt("ingrese la edad"));
+
+if(edad>18){
+    document.write("usted ya puede conducir");
+}
