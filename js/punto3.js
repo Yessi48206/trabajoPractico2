653a14512f2de7400b477eac2e92ca5c9c2f6bee
Yessica Juarez <yessica48206@gmail.com>
2022-07-31 12:52:12 -0300

    hice el punto 3 del tp 2

---
+++ js/punto3.js
@@ -0,0 +1,19 @@
+/*3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” 
+deben mostrarse todas las cadenas concatenadas con un guión -.*/
+
+let text;
+let palabra;
+do{
+    text= prompt("ingrese texto");
+    console.log(text);
+    document.write(text + "-");
+}while(confirm("quiere seguir ingresando") == true)
+//cadena concatenada con guion
+
+/*if (confirm("Press a button!") == true) {
+  text = "You pressed OK!";
+  document.write(text);
+} else {
+  text = "You canceled!";
+  document.write(text);
+}*/
